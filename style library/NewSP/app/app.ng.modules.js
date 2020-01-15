c88fc680bdf80673b75e25312e8b58b5282fdd7b
junreyd <42532126+junreyd@users.noreply.github.com>
2020-01-15 16:55:46 +0800

    adding route in master page and controller in modules

--- style library/NewSP/app/app.ng.modules.js
+++ style library/NewSP/app/app.ng.modules.js
@@ -1,1 +1,35 @@
-﻿var newSp = angular.module('newSp', []);
+﻿var newSp = angular.module('newSp', ['ngRoute']);
+
+newSp.config(function ($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'home.aspx/first.html',
+            controller: 'FirstController'
+        })
+
+        .when('/blog', {
+            templateUrl: 'home.aspx/second.html',
+            controller: 'SecondController'
+        })
+
+        .when('/about', {
+            templateUrl: 'home.aspx/third.html',
+            controller: 'ThirdController'
+        })
+
+        .otherwise({ redirectTo: '/' });
+});
+
+
+newSp.controller('FirstController', function ($scope) {
+    $scope.message = 'Hello from FirstController';
+});
+
+newSp.controller('SecondController', function ($scope) {
+    $scope.message = 'Hello from SecondController';
+});
+
+newSp.controller('ThirdController', function ($scope) {
+    $scope.message = 'Hello from ThirdController';
+});
