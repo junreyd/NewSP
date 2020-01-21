1183eff7ab065f7dc79d8934e331f75b65c6a0d5
junreyd <42532126+junreyd@users.noreply.github.com>
2020-01-21 16:51:06 +0800

    create controller to separate from modules

---
+++ style library/NewSP/app/app.ng.controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    newSp.controller('FirstController', ['$scope', function ($scope, svc) {
+        $scope.message = 'Hello from FirstController';
+        console.log("FirstController")
+        console.log(svc);
+    }]);
+
+    newSp.controller('SecondController', function ($scope) {
+        $scope.message = 'Hello from SecondController';
+        console.log("SecondController")
+    });
+
+    newSp.controller('ThirdController', function ($scope) {
+        $scope.message = 'Hello from ThirdController';
+        console.log("ThirdController")
+    });
+
+
+})();
--- style library/NewSP/app/app.ng.modules.js
+++ style library/NewSP/app/app.ng.modules.js
@@ -22,29 +22,3 @@ newSp.config(['$qProvider', '$routeProvider', function ($qProvider, $routeProvid
 
         .otherwise({ redirectTo: '/' });
 }]);
-
-
-newSp.controller('FirstController', ['$scope', function ($scope, svc) {
-    $scope.message = 'Hello from FirstController';
-    console.log("FirstController")
-    console.log(svc);
-}]);
-
-newSp.controller('SecondController', function ($scope) {
-    $scope.message = 'Hello from SecondController';
-    console.log("SecondController")
-});
-
-newSp.controller('ThirdController', function ($scope) {
-    $scope.message = 'Hello from ThirdController';
-    console.log("ThirdController")
-});
-
-
-//newSp.controller('FirstController', ['$scope', '$location', '$log',
-//    function ($scope, $location, $log) {
-
-//        $log.info($location.path());
-
-//    }
-//])
